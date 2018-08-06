88e5f0ff003ed37fc964aee6dc2eb14a07f8dbc6
gks7757 <gks7757@nate.com>
2018-08-06 16:42:11 +0900

    update

--- js/init.js
+++ js/init.js
@@ -286,4 +286,48 @@ jQuery(function($){
     marker.setMap(map);
 
 
+// twosome app
+
+    $('.tab_control li').on('click',function(e) {
+        e.preventDefault();
+        $('.tab_control li').removeClass('on');
+        $(this).addClass('on');
+    });
+
+
+
+// function show_more(){
+//     var page = $("#menu_page").val();
+//     page = page*1;
+//     $.ajax({
+//         type: 'POST',
+//         url: '../inc/ajax_brand.php?gubun=menu_more&product_cate=7&chked_val=&page='+page,                      
+//         success: function(result) {
+//             if(result !="none"){ //con_btn이 디스플레이 논이 아닐때
+//                 $("#menu_page").val(page+1);
+//                 $("#menu_ul").append(result);
+//             }else{
+//                 $(".con_btn").hide();
+//             };
+//         }
+//     });         
+// }
+
+// function change_cate(){
+//     var chked_val = "";
+//       $(":checkbox[name='chkList']:checked").each(function(pi,po){
+//         if( po.value=="all" || chked_val =="all"){
+//             chked_val="all";
+//         }else{
+//             chked_val += po.value + ",";
+//         }
+//     });
+    
+//     location.replace("/contents/drink.html?chked_val="+chked_val+"#blockcate"); 
+// }
+
+
+
+
+
 });
